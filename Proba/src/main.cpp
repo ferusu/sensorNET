c10f863572287f06dbf94da3c0abac44c827f23d
ferusu <ferusun@gmail.com>
2019-02-14 05:48:23 +0100

    Added the libraries and the framework to start developing the network

--- Proba/src/main.cpp
+++ Proba/src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+//#include <WiFiManager.h>
 
+// Configuración de la WiFi generada
+const char *ssid = "canguele";
+const char *password = "password";
+int incomingByte = 0; // almacenar el dato serie
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(10);
+  
+  WiFi.mode(WIFI_AP);
+  while(!WiFi.softAP(ssid, password))
+  {
+   Serial.println(".");
+    delay(100);
+  }
+  
+  Serial.print("Iniciado AP ");
+  Serial.println(ssid);
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.softAPIP());
+  pinMode(D4, OUTPUT);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(D4, HIGH);   // turn the LED on (HIGH is the voltage level)
+if (Serial.available() > 0) {
+// lee el byte de entrada:
+incomingByte = Serial.read();
+//lo vuelca a pantalla
+Serial.print("He recibido: "); Serial.println(incomingByte,DEC);
+} 
+  }
