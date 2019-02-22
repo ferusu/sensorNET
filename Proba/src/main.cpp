5a7f49ca1b0c364bf2afa28e03257237dcbe878a
ferusu <ferusun@gmail.com>
2019-02-22 04:50:28 +0100

    Set a UDP connection port and a small protocol to send a test message

--- Proba/src/main.cpp
+++ Proba/src/main.cpp
@@ -1,116 +1,57 @@
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
-#include <ESP8266WebServer.h>
-#include <DNSServer.h>
 #include <WiFiManager.h>
-/* ESP8266 Webserver obtaining arguments from a connected client
- The 'MIT License (MIT) Copyright (c) 2016 by David Bird'. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, 
- distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
- following conditions: 
-   The above copyright ('as annotated') notice and this permission notice shall be included in all copies or substantial portions of the Software and where the
-   software use is visible to an end-user.
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-See more at http://dsbird.org.uk
-*/
+IPAddress ip0;
+WiFiUDP Udp;
+unsigned int localPort = 2399;      // local port to listen on
 
+char packetBuffer[255]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "papuda";       // a string to send back
 
-WiFiClient client;
-ESP8266WebServer server(80); // Start server on port 80 (default for a web-browser, change to your requirements, e.g. 8080 if your Router uses port 80
-                             // To access server from the outsid of a WiFi network e.g. ESP8266WebServer server(8266); and then add a rule on your Router that forwards a
-                             // connection request to http://your_network_ip_address:8266 to port 8266 and view your ESP server from anywhere.
-                             // Example http://g6ejd.uk.to:8266 will be directed to http://192.168.0.40:8266 or whatever IP address your router gives to this server
+IPAddress ip(192, 168, 0, 177);
+IPAddress ip1(0, 0, 0, 0);
+IPAddress ip2(0, 0, 0, 0);
+IPAddress ip3(0, 0, 0, 0);
+IPAddress ip4(0, 0, 0, 0);
 
-String Argument_Name, Clients_Response1, Clients_Response2;
-
-void ShowClientResponse() {
-  String webpage;
-  webpage =  "<html>";
-   webpage += "<head><title>ESP8266 Input Example</title>";
-    webpage += "<style>";
-     webpage += "body { background-color: #E6E6FA; font-family: Arial, Helvetica, Sans-Serif; Color: blue;}";
-    webpage += "</style>";
-   webpage += "</head>";
-   webpage += "<body>";
-    webpage += "<h1><br>ESP8266 Server - This was what the client sent</h1>";
-    webpage += "<p>Name received was: " + Clients_Response1 + "</p>";
-    webpage += "<p>Address received was: " + Clients_Response2 + "</p>";
-   webpage += "</body>";
-  webpage += "</html>";
-  server.send(200, "text/html", webpage); // Send a response to the client asking for input
-}
-
-void HandleClient() {
-  String webpage;
-  webpage =  "<html>";
-   webpage += "<head><title>ESP8266 Input Example</title>";
-    webpage += "<style>";
-     webpage += "body { background-color: #E6E6FA; font-family: Arial, Helvetica, Sans-Serif; Color: blue;}";
-    webpage += "</style>";
-   webpage += "</head>";
-   webpage += "<body>";
-    webpage += "<h1><br>ESP8266 Server - Getting input from a client</h1>";  
-    String IPaddress = WiFi.localIP().toString();
-    webpage += "<form action='http://"+IPaddress+"' method='POST'>";
-     webpage += "&nbsp;&nbsp;&nbsp;&nbsp;Please enter your Name:<input type='text' name='name_input'><BR>";
-     webpage += "Please enter your Address:<input type='text' name='address_input'>&nbsp;<input type='submit' value='Enter'>"; // omit <input type='submit' value='Enter'> for just 'enter'
-    webpage += "</form>";
-   webpage += "</body>";
-  webpage += "</html>";
-  server.send(200, "text/html", webpage); // Send a response to the client asking for input
-  if (server.args() > 0 ) { // Arguments were received
-    for ( uint8_t i = 0; i < server.args(); i++ ) {
-      Serial.print(server.argName(i)); // Display the argument
-      Argument_Name = server.argName(i);
-      if (server.argName(i) == "name_input") {
-        Serial.print(" Input received was: ");
-        Serial.println(server.arg(i));
-        Clients_Response1 = server.arg(i);
-        // e.g. range_maximum = server.arg(i).toInt();   // use string.toInt()   if you wanted to convert the input to an integer number
-        // e.g. range_maximum = server.arg(i).toFloat(); // use string.toFloat() if you wanted to convert the input to a floating point number
-      }
-      if (server.argName(i) == "address_input") {
-        Serial.print(" Input received was: ");
-        Serial.println(server.arg(i));
-        Clients_Response2 = server.arg(i);
-        // e.g. range_maximum = server.arg(i).toInt();   // use string.toInt()   if you wanted to convert the input to an integer number
-        // e.g. range_maximum = server.arg(i).toFloat(); // use string.toFloat() if you wanted to convert the input to a floating point number
-      }
-    }
-  }
-}
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  //WiFiManager intialisation. Once completed there is no need to repeat the process on the current board
-  WiFiManager wifiManager;
-  // New OOB ESP8266 has no Wi-Fi credentials so will connect and not need the next command to be uncommented and compiled in, a used one with incorrect credentials will
-  // so restart the ESP8266 and connect your PC to the wireless access point called 'ESP8266_AP' or whatever you call it below in ""
-  // wifiManager.resetSettings(); // Command to be included if needed, then connect to http://192.168.4.1/ and follow instructions to make the WiFi connection
-  // Set a timeout until configuration is turned off, useful to retry or go to sleep in n-seconds
-  wifiManager.setTimeout(180);
-  //fetches ssid and password and tries to connect, if connections succeeds it starts an access point with the name called "ESP8266_AP" and waits in a blocking loop for configuration
-  if(!wifiManager.autoConnect("ESP8266_AP")) {
-    Serial.println("failed to connect and timeout occurred");
-    delay(3000);
-    ESP.reset(); //reset and try again
-    delay(5000);
-  }
-  // At this stage the WiFi manager will have successfully connected to a network, or if not will try again in 180-seconds
-  //----------------------------------------------------------------------------------------------------------------------
-  Serial.println("WiFi connected..");
-  server.begin(); Serial.println("Webserver started..."); // Start the webserver
-  Serial.print("Use this URL to connect: http://");// Print the IP address
-  Serial.print(WiFi.localIP());Serial.println("/");
-  // NOTE: You must use the IP address assigned to YOUR Board when printed/displayed here on your serial port
-  // that's the address you must use, not the one I used !
-  
-  // Next define what the server should do when a client connects
-  server.on("/", HandleClient); // The client connected with no arguments e.g. http:192.160.0.40/
-  server.on("/result", ShowClientResponse);
+  Serial.println();
+  WiFi.config(ip, ip1, ip2, ip3, ip4);
+  Serial.print("Setting soft-AP ... ");
+  Serial.println(WiFi.softAP("SensorNET", "123456789") ? "Ready" : "Failed!");
+  Udp.begin(localPort);
 }
 
-void loop() {
-  server.handleClient();
+void loop()
+{
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+  if((millis()&0xFFFF)==20000)
+  {
+    Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+    ip = WiFi.localIP();
+    Serial.println(ip);
+  }
 }
