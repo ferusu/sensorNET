176e6988cdec28007761c4b155dba5c0d4610221
ferusu <ferusun@gmail.com>
2019-03-08 01:51:43 +0100

    Added the code to implement the software serial feature

---
+++ DataAcquirer/lib/dahal_sser/src/dahal_sser.h
@@ -0,0 +1,20 @@
+
+
+#ifndef dahal_sser_h
+#define dahal_sser_h
+
+#include <ESP8266WiFi.h>
+
+class DahalSser
+{
+  public:
+    DahalSser(int receivePin, int transmitPin);
+    bool availableData();
+    char read();
+  private:
+    bool _dahalSserInitialized;
+};
+
+void DahalSserInit (void);
+
+#endif
