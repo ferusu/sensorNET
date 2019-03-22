e9de4fd0f1060ecb7020fd4fa9d920f871a1308f
ferusu <ferusun@gmail.com>
2019-03-22 03:13:30 +0100

    Added the tmr module

--- Hub/lib/hhal_evt/src/hhal_evt.h
+++ Hub/lib/hhal_evt/src/hhal_evt.h
@@ -20,12 +20,52 @@
 /*         Typedef of exported structures and enumerations       */
 /*****************************************************************/
 
+typedef enum
+{
+    SUCCESS = 0,
+    ERROR = 1,
+    NOT_KNOWN_EVENT_TYPE = 2,
+    EVENT_QUEUE_OVERLOAD = 3,
+    NOT_EVENT = 4,
+    NOT_KNOWN_ORDER
+}eventHandlingResult_t;
 
+typedef enum
+{
+    DAHAL_EVENT_TYPE_NOT_EVENT = 0,
+    DAHAL_EVENT_TYPE_WIFI,
+    DAHAL_EVENT_TYPE_SOFTWARE_SERIAL,
+    DAHAL_EVENT_TYPE_TIMER
+}eventType_t;
+
+typedef enum
+{
+    MAIN_STATE_MACHINE_TIMER = 0,
+    HEARTBEAT_TIMER
+}timerType_t;
+
+
+typedef struct
+{
+    timerType_t timerType;
+    uint32_t timestamp;
+}timer2_t;
+
+
+typedef struct
+{
+    eventType_t eventType;
+    timer2_t timer;
+}event_t;
 
 /*****************************************************************/
 /*                  Public Function Prototypes                   */
 /*****************************************************************/
 
+void HhalEvtInit(void);
 
+bool HhalEvtGet(event_t *eventOutput);
+
+eventHandlingResult_t HhalEvtQueuePut (event_t eventInput);
 
 #endif
--- Hub/lib/hhal_tmr/src/hhal_tmr.cpp
+++ Hub/lib/hhal_tmr/src/hhal_tmr.cpp
@@ -20,6 +20,12 @@
 #include <hhal_tmr.h>
 
 /******************************************************************/
+/*                     Definition of objects                      */
+/******************************************************************/
+
+
+
+/******************************************************************/
 /*            Definition of local symbolic constants              */
 /******************************************************************/
 
@@ -47,26 +53,76 @@
 /*                 Private Variable Declaration                  */
 /*****************************************************************/
 
-
+os_timer_t softwareTimer;
+static bool timerInit = false;
+static uint16_t prescaler;
+static event_t event;
 
 /*****************************************************************/
 /*                  Local Function Prototypes                    */
 /*****************************************************************/
 
-
+void timerCallback(void *pArg);
 
 
 /*****************************************************************/
 /*                  Local Function Declaration                   */
 /*****************************************************************/
 
+void timerCallback(void *pArg)
+{
+    static uint32_t mainStatMachineTimestamp = 0;
+    static uint32_t heartbeatTimestamp = 0;
+    static uint16_t prescalerCounter;
 
+    event.timer.timerType = HEARTBEAT_TIMER;
+    event.timer.timestamp = mainStatMachineTimestamp;
+    HhalEvtQueuePut (event);
+    heartbeatTimestamp++;
+    prescalerCounter++;
+    if (prescalerCounter>=prescaler)
+    {
+        event.timer.timerType = MAIN_STATE_MACHINE_TIMER;
+        event.timer.timestamp = mainStatMachineTimestamp;
+        HhalEvtQueuePut (event);
+        mainStatMachineTimestamp++;
+        prescalerCounter = 0;
+        digitalWrite(D4, !digitalRead(D4));
+    }
+}
 
 /*****************************************************************/
 /*                  Public Function Declaration                  */
 /*****************************************************************/
 
+void HhalTmrInit (void)
+{
+    os_timer_setfn(&softwareTimer, timerCallback, NULL);
+    event.eventType =  DAHAL_EVENT_TYPE_TIMER;
+    timerInit = true;
+}
 
-
-
-
+void HhalTmrSet (timerType_t timerType, uint16_t period)
+{
+    static uint16_t mainStateMachinePeriod = 0;
+    static uint16_t heartbeatPeriod = 0;
+    if(timerInit)
+    {
+        switch(timerType)
+        {
+            case MAIN_STATE_MACHINE_TIMER:
+                if ((period>heartbeatPeriod)&&(heartbeatPeriod>0))
+                {
+                    prescaler = period/heartbeatPeriod;
+                    mainStateMachinePeriod = heartbeatPeriod*prescaler;
+                    Serial.println(mainStateMachinePeriod);
+                }
+                break;
+            case HEARTBEAT_TIMER:
+                heartbeatPeriod = period;
+                Serial.println(heartbeatPeriod);
+                break;
+        }
+        os_timer_arm(&softwareTimer, (int)heartbeatPeriod, true);
+    }
+}
